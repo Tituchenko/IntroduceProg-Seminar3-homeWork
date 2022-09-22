51b21d107b6d2d88d05305406e206327890ec8de
Ilya Tituchenko <titu4enko@mail.ru>
2022-09-22 22:02:36 +0300

    13 solved biutiful

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 19
+//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+
+int n=5; // Эта наша разрядность, работает с любой
+Console.Write ($"Введите {n}-значное число: "); //Пусть у нас добросовестный пользователь (без проверки разряда)
+int num=int.Parse (Console.ReadLine()!);
+bool polynom=true; //Пусть у нас полином, попробуем опровергнуть
+for (int i=1;i<=n/2;i++) { //Проверим от начала числа к середине и от конца к серидине чтобы все были равны, 
+//пойдем как бы одновременно зеркально
+    int leftSymbol=num/Convert.ToInt32(Math.Pow(10,n-i)); // Цифры от начала к текущему i
+    int rightSymbol=num%Convert.ToInt32(Math.Pow(10,i)); // Цифры от конца к текущему i
+    if (i>1) //Если это не первый шаг, то у нас будет несколько цифр, обрежим
+    {
+        leftSymbol=leftSymbol%10; //Интересна последняя
+        rightSymbol=rightSymbol/Convert.ToInt32(Math.Pow(10,i-1)); //Интересна первая
+    }
+    if (leftSymbol!=rightSymbol) {
+        polynom=false;
+        break; // Зачем идти дальше
+    }
+}
+if (polynom) {
+    Console.WriteLine ($"{num} -> да");
+}
+else {
+    Console.WriteLine ($"{num} -> нет");
+}
+
+
+
